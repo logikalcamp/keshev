b2d762fdc6f46c1fac8ba4d795a20644552f3fcb
logikalcamp <logikalcamp@gmail.com>
2020-11-19 11:41:17 +0200

    new

--- pages/_document.js
+++ pages/_document.js
@@ -20,6 +20,106 @@ export default class extends Document {
           <meta name='format-detection' content='telephone=no' />
           <meta name='mobile-web-app-capable' content='yes' />
           <meta name='theme-color' content='#FFFFFF' />
+          <link
+            rel="apple-touch-startup-image"
+            media="screen and (device-width: 320px) and (device-height: 568px) and (-webkit-device-pixel-ratio: 2) and (orientation: landscape)"
+            href="/icons/apple-icon-180x180-dunplab-manifest-28509.png"
+          />
+          <link
+            rel="apple-touch-startup-image"
+            media="screen and (device-width: 375px) and (device-height: 812px) and (-webkit-device-pixel-ratio: 3) and (orientation: landscape)"
+            href="/icons/apple-icon-180x180-dunplab-manifest-28509.png"
+          />
+          <link
+            rel="apple-touch-startup-image"
+            media="screen and (device-width: 414px) and (device-height: 896px) and (-webkit-device-pixel-ratio: 2) and (orientation: landscape)"
+            href="/icons/apple-icon-180x180-dunplab-manifest-28509.png"
+          />
+          <link
+            rel="apple-touch-startup-image"
+            media="screen and (device-width: 414px) and (device-height: 896px) and (-webkit-device-pixel-ratio: 2) and (orientation: portrait)"
+            href="/icons/apple-icon-180x180-dunplab-manifest-28509.png"
+          />
+          <link
+            rel="apple-touch-startup-image"
+            media="screen and (device-width: 375px) and (device-height: 667px) and (-webkit-device-pixel-ratio: 2) and (orientation: landscape)"
+            href="/icons/apple-icon-180x180-dunplab-manifest-28509.png"
+          />
+          <link
+            rel="apple-touch-startup-image"
+            media="screen and (device-width: 414px) and (device-height: 896px) and (-webkit-device-pixel-ratio: 3) and (orientation: portrait)"
+            href="/icons/apple-icon-180x180-dunplab-manifest-28509.png"
+          />
+          <link
+            rel="apple-touch-startup-image"
+            media="screen and (device-width: 414px) and (device-height: 736px) and (-webkit-device-pixel-ratio: 3) and (orientation: landscape)"
+            href="/icons/apple-icon-180x180-dunplab-manifest-28509.png"
+          />
+          <link
+            rel="apple-touch-startup-image"
+            media="screen and (device-width: 375px) and (device-height: 812px) and (-webkit-device-pixel-ratio: 3) and (orientation: portrait)"
+            href="/icons/apple-icon-180x180-dunplab-manifest-28509.png"
+          />
+          <link
+            rel="apple-touch-startup-image"
+            media="screen and (device-width: 414px) and (device-height: 736px) and (-webkit-device-pixel-ratio: 3) and (orientation: portrait)"
+            href="/icons/apple-icon-180x180-dunplab-manifest-28509.png"
+          />
+          <link
+            rel="apple-touch-startup-image"
+            media="screen and (device-width: 1024px) and (device-height: 1366px) and (-webkit-device-pixel-ratio: 2) and (orientation: landscape)"
+            href="/icons/apple-icon-180x180-dunplab-manifest-28509.png"
+          />
+          <link
+            rel="apple-touch-startup-image"
+            media="screen and (device-width: 414px) and (device-height: 896px) and (-webkit-device-pixel-ratio: 3) and (orientation: landscape)"
+            href="/icons/apple-icon-180x180-dunplab-manifest-28509.png"
+          />
+          <link
+            rel="apple-touch-startup-image"
+            media="screen and (device-width: 834px) and (device-height: 1112px) and (-webkit-device-pixel-ratio: 2) and (orientation: landscape)"
+            href="/icons/apple-icon-180x180-dunplab-manifest-28509.png"
+          />
+          <link
+            rel="apple-touch-startup-image"
+            media="screen and (device-width: 375px) and (device-height: 667px) and (-webkit-device-pixel-ratio: 2) and (orientation: portrait)"
+            href="/icons/apple-icon-180x180-dunplab-manifest-28509.png"
+          />
+          <link
+            rel="apple-touch-startup-image"
+            media="screen and (device-width: 1024px) and (device-height: 1366px) and (-webkit-device-pixel-ratio: 2) and (orientation: portrait)"
+            href="/icons/apple-icon-180x180-dunplab-manifest-28509.png"
+          />
+          <link
+            rel="apple-touch-startup-image"
+            media="screen and (device-width: 834px) and (device-height: 1194px) and (-webkit-device-pixel-ratio: 2) and (orientation: landscape)"
+            href="/icons/apple-icon-180x180-dunplab-manifest-28509.png"
+          />
+          <link
+            rel="apple-touch-startup-image"
+            media="screen and (device-width: 834px) and (device-height: 1112px) and (-webkit-device-pixel-ratio: 2) and (orientation: portrait)"
+            href="/icons/apple-icon-180x180-dunplab-manifest-28509.png"
+          />
+          <link
+            rel="apple-touch-startup-image"
+            media="screen and (device-width: 320px) and (device-height: 568px) and (-webkit-device-pixel-ratio: 2) and (orientation: portrait)"
+            href="/icons/apple-icon-180x180-dunplab-manifest-28509.png"
+          />
+          <link
+            rel="apple-touch-startup-image"
+            media="screen and (device-width: 834px) and (device-height: 1194px) and (-webkit-device-pixel-ratio: 2) and (orientation: portrait)"
+            href="/icons/apple-icon-180x180-dunplab-manifest-28509.png"
+          />
+          <link
+            rel="apple-touch-startup-image"
+            media="screen and (device-width: 768px) and (device-height: 1024px) and (-webkit-device-pixel-ratio: 2) and (orientation: landscape)"
+            href="/icons/apple-icon-180x180-dunplab-manifest-28509.png"
+          />
+          <link
+            rel="apple-touch-startup-image"
+            media="screen and (device-width: 768px) and (device-height: 1024px) and (-webkit-device-pixel-ratio: 2) and (orientation: portrait)"
+            href="/icons/apple-icon-180x180-dunplab-manifest-28509.png"
+          />
           {/* TIP: set viewport head meta tag in _app.js, otherwise it will show a warning */}
           {/* <meta name='viewport' content='minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no, viewport-fit=cover' /> */}
           
